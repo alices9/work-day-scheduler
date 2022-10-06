310182f309bf934161fe900bf7ce2561b6e59373
Alice Shao <a1ice_shao@yahoo.com>
2022-10-06 19:32:58 -0400

    added color coding function basis

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+
+console.log($('hour'));
+
 var todaysDate = moment().format("MMM Do YY");
 $("#currentDay").text(todaysDate);
 
 var currentHour = moment().hour();
 console.log(currentHour);
 
+
+$(".row").each(function() {
+    if (currentHour === $(this).attr("id")){
+        $(this).addClass("present");
+    } else if (currentHour > $(this).attr("id")){
+        $(this).addClass("past");
+    } else {
+        $(this).addClass("future");}
+
+}
+)
